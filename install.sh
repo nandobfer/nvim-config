a629538829f0cf61a60777d1f939022105189954
nandobfer <nandobfer@yahoo.com.br>
2026-06-01 01:30:49 +0200

    adicionado plugin de cor

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage && \
+chmod u+x nvim-linux-x86_64.appimage && \
+sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
