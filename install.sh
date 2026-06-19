00d568682ecd0d564df38367bf0dae0bf08abd6b
nandoburgos <nandobfer@yahoo.com.br>
2026-06-19 09:57:40 -0300

    adicionando nvim localmente

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage && \
-chmod u+x nvim-linux-x86_64.appimage && \
-sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+# Garante que o diretório de binários do usuário existe
+mkdir -p ~/.local/bin
+
+# Baixa o AppImage
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+
+# Torna executável
+chmod u+x nvim-linux-x86_64.appimage
+
+# Move para o diretório local do usuário com o nome 'nvim'
+mv nvim-linux-x86_64.appimage ~/.local/bin/nvim
